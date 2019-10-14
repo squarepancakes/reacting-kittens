e00b19176a4a923e96115e2f1a1a9f1931a2671c
squarepancakes <goh.shuyun@gmail.com>
2019-10-14 15:21:29 +0800

    added adopt kitten button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			kittens: [],
+			owners: [],
+			isLoggedIn: false
+		};
+	}
+	fetchKittens = () => {
+		//some fetching code
+		const kittens = [{ name: "fluffy", age: 2, sex: "female" }];
+		const url = "http://localhost:5000/kittens";
+		axios
+			.get(url)
+			.then(res => {
+				this.setState({ kittens: res.data });
+			})
+			.catch(err => {
+				console.error(err);
+				this.setState({ kittens: [] });
+			});
+
+		this.setState({ kittens });
+	};
+
+	fetchOwners = () => {
+		const url = "http://localhost:5000/owners/papaya";
+		axios
+			.get(url, { withCredentials: true })
+			.then(res => {
+				this.setState({ owners: [res.data] });
+			})
+			.catch(err => {
+				console.error(err);
+				this.setState({ owners: [] });
+			});
+	};
+
+	loginHandler = () => {
+		const url = "http://localhost:5000/owners/login";
+		// url, payload, options
+		axios
+			.post(
+				url,
+				{
+					username: "papaya",
+					password: "1234567"
+				},
+				{ withCredentials: true }
+			)
+			.then(() => {
+				this.setState({ isLoggedIn: true });
+			})
+			.catch(err => {
+				console.error(err);
+				this.setState({ isLoggedIn: false });
+			});
+	};
+
+	logoutHandler = () => {
+		const url = "http://localhost:5000/owners/logout";
+		axios
+			.post(url, {}, { withCredentials: true })
+			.then(() => {
+				this.setState({ isLoggedIn: false });
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	};
+
+	adoptKitten = name => {
+		const url = "http://localhost:5000/kittens/" + name;
+		axios
+			.delete(url, { withCredentials: true })
+			.then(res => {
+				console.log(res);
+				this.fetchKittens();
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<div className="access">
+					<button onClick={this.loginHandler}>Login</button>
+					<button onClick={this.logoutHandler}>Logout</button>
+					<p>
+						{this.state.isLoggedIn
+							? "You're logged in"
+							: "You're not logged in"}
+					</p>
+				</div>
+				<button onClick={this.fetchOwners}>
+					Get username of owner (protected)!
+				</button>
+				{this.state.owners.map(owner => {
+					return <p key={owner.fullName}>{owner.fullName}</p>;
+				})}
+				<button onClick={this.fetchKittens}>Get me all kittens!</button>
+				{this.state.kittens.map(kitten => {
+					return (
+						<div key={kitten.name}>
+							<p>Name: {kitten.name}</p>
+							<p>{kitten.sex}</p>
+							<p>{kitten.age} years old</p>
+							<button onClick={() => this.adoptKitten(kitten.name)}>
+								Adopt kitten!
+							</button>
+						</div>
+					);
+				})}
+
+				<footer>No kittens were harmed in the making of said site</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
